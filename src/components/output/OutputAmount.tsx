3850d6b2df0d6d43362a1cdcde2ef9b290060156
Appz-dk <patrick.fyhn@gmail.com>
2023-03-18 14:04:22 +0100

    Fixed OutputAmount to scale fontSize based on length

--- src/components/output/OutputAmount.tsx
+++ src/components/output/OutputAmount.tsx
@@ -4,6 +4,9 @@ type OutputAmountProps = {
 };
 
 const OutputAmount: React.FC<OutputAmountProps> = ({ outputType, outputAmount }) => {
+  // Scales down the fontsize if output length gets too long, so it doesnt break the layout.
+  // The numbers are just magic numbers, found by trial and error.
+  const fontSize = Math.max(Math.min(1.9, 16 / outputAmount.length), 0.8);
   return (
     <div className="output__amount-container">
       <div className="output__type">
@@ -12,7 +15,7 @@ const OutputAmount: React.FC<OutputAmountProps> = ({ outputType, outputAmount })
         <p aria-hidden>/ person</p>
       </div>
       <div className="output__amount">
-        <p>${outputAmount}</p>
+        <p style={{ fontSize: `${fontSize}rem` }}>${outputAmount}</p>
       </div>
     </div>
   );
